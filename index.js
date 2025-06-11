047f2d76d844c2f94a1ab8acfddff51ffd05709c
JSebastianC26 <jscardona026@gmail.com>
2025-06-10 21:35:45 -0500

    database conection

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 import express from 'express';
- 
-const app = express()
-
-const PORT = 3000;
+import {config} from 'dotenv';
+import pg from 'pg';
+config();
+const app = express();
+const pool = new pg.Pool({
+    connectionString: process.env.DATABASE_URL, 
+    ssl: true
+})
+const PORT = process.env.PORT;
 
 app.get('/', (req, res) => {
     res.send("Hello world!")
+})
+
+app.get('/ping', async (req, res) =>{
+    const result = await pool.query('select now()');
+
+    return res.json(result.rows[0])
 })
 
 app.listen(PORT, () => {
