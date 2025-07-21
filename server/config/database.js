def7536889a3cd0540ce4624f832f7c5134b4bb4
JSebastianC26 <65996169+JSebastianC26@users.noreply.github.com>
2025-07-21 14:27:30 -0500

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -6,7 +6,16 @@ const pool = new pg.Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
-    }
+    },
+    // Forzar IPv4
+    host: 'db.cfjsprcnmoeshfakokif.supabase.co',
+    port: 5432, // o el puerto que te dé Supabase
+    database: 'postgres',
+    user: 'postgres',
+    password: process.env.DB_PASSWORD,
+    // Configuraciones adicionales
+    connectionTimeoutMillis: 30000,
+    idleTimeoutMillis: 30000,
 });
 
 export default pool;
